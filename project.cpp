e7477d22d8f29efbe0485b032ef8154a696b425a
Ahmad <sa8846758@gmail.com>
2024-05-16 12:13:50 +0500

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	
+	int size;
+	cout<<"Size:     ";
+	cin>>size;
+	
+	int* array = new int[size];
+	
+	for(int i=0; i<size; i++) {
+		cout<<"Element "<<i+1<<":   ";
+		cin>>array[i];
+	}
+	
+	int sum = 0;
+	
+	for(int i=0; i<size; i++) {
+		sum+=array[i];
+	}
+	
+	system("cls");
+	
+	cout<<"Sum of all values is: "<<sum;
+	
+}
